73b78bb009c3dea423c78d68af699fcb7dce966c
ZayMMM <zaymaungmaungmyint.info@gmail.com>
2023-03-19 20:52:25 +0630

    Looping, Array

---
+++ java-basic/src/com/mm/bee/tech/DoWhileLoop.java
@@ -0,0 +1,15 @@
+package com.mm.bee.tech;
+
+public class DoWhileLoop {
+	
+	public static void main(String[] args) {
+		
+		int count = 0;
+		
+		do {
+			System.out.println(count);
+			count++;
+		}while(count < 10);
+	}
+
+}
---
+++ java-basic/src/com/mm/bee/tech/WhileLoopDemo.java
@@ -0,0 +1,16 @@
+package com.mm.bee.tech;
+
+public class WhileLoopDemo {
+
+	public static void main(String[] args) {
+
+		int count = 0;
+
+		while (count < 10) {
+			// statement
+			System.out.println(count);
+			count++;
+		}
+	}
+
+}
